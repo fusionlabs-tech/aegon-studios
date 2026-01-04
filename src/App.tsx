850b23039d62666418b5213a36a4dae1708db9d6
Desmond Obisi <desmondcutest@yahoo.com>
2026-01-04 08:48:21 +0000

    Generated by Spark: Fix all reported errors.

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,30 @@ import { Clients } from '@/components/Clients'
 import { Testimonials } from '@/components/Testimonials'
 import { Contact } from '@/components/Contact'
 import { ScrollProgress } from '@/components/ScrollProgress'
+import { Toaster } from '@/components/ui/sonner'
 import { motion } from 'framer-motion'
 
 function App() {
   return (
-    <motion.div 
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-      className="relative"
-    >
-      <ScrollProgress />
-      <Hero />
-      <Showreel />
-      <Portfolio />
-      <Services />
-      <About />
-      <Clients />
-      <Testimonials />
-      <Contact />
-    </motion.div>
+    <>
+      <motion.div 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="relative"
+      >
+        <ScrollProgress />
+        <Hero />
+        <Showreel />
+        <Portfolio />
+        <Services />
+        <About />
+        <Clients />
+        <Testimonials />
+        <Contact />
+      </motion.div>
+      <Toaster />
+    </>
   )
 }
 
